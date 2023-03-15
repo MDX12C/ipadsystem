6e0b8b6c5973833e60f00b31256fa0e4a76b6888
banahaker <pn0818x@outlook.com>
2023-03-15 22:14:49 +0800

    feat: key generating algorithm

--- src/router/register.js
+++ src/router/register.js
@@ -1,14 +1,21 @@
 import { json, Router } from "express";
 import { register } from "../model/ipaddatabase.js";
+import { keyGen } from "../utils/keyAlgorithm.js";
 
 const router = Router();
 router.use(json());
 
 router.post("/register", async (req, res) => {
   const body = req.body;
+
+  if (body.key != keyGen(body.schoolNumber)) {
+    return res.json({
+      status: "wrongKey",
+    });
+  }
   try {
     await register(
-      body.schoolNumber,
+      String(body.schoolNumber).toLowerCase(),
       body.classNumber,
       body.name,
       body.ipadNumber
---
+++ src/utils/keyAlgorithm.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+
+export const keyGen = (schoolNumber) => {
+  dotenv.config();
+  const globalKey = process.env.CLASS_12_KEY;
+  let key = "";
+  for (let i = 0; i < 6; i++) {
+    key =
+      key +
+      String.fromCodePoint(
+        (
+          (schoolNumber[i].codePointAt(0) + globalKey[i].codePointAt(0)) /
+          2
+        ).toFixed()
+      );
+  }
+  return key;
+};
+
+// console.log(keyGen("11s505"));
